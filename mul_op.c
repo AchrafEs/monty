bf54fb2b11942fad2ced750e75d9090525d17389
AchrafEs <achraf.es2110@gmail.com>
2023-06-20 14:37:25 +0000

    added the function mul

---
+++ mul_op.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - A function that multiplies the second top
+ * element of the stack with the top element of the stack.
+ * @line_number: the number of the line.
+ *
+ * Return: Nothing
+ */
+void mul(int line_number)
+{
+	if (top < 1)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	stack[top - 1] *= stack[top];
+	top--;
+}
