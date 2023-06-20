ea6515d252f65d1b3293e65dfc3d8efb521a2889
AchrafEs <achraf.es2110@gmail.com>
2023-06-20 14:24:10 +0000

    Function to subtract the top element from the second top element of the stack

---
+++ sub_op.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * sub - A function that subtracts the top element of
+ * the stack from the second top element of the stack
+ * @line_number: the number of the line.
+ *
+ * Return: Void.
+ */
+void sub(int line_number)
+{
+	if (top < 1)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	stack[top - 1] -= stack[top];
+	top--;
+}
