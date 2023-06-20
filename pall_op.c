29ced11643e369a4b862f828e26afa5b7e450edb
AchrafEs <achraf.es2110@gmail.com>
2023-06-20 13:42:18 +0000

    added the function that prints all the values on the stack

---
+++ pall_op.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * pall - a function that prints all values
+ * on the stack
+ *
+ * Return: Void
+ */
+void pall()
+{
+	int i;
+
+	if (top == -1)
+	{
+		return;
+	}
+	for (i = top; i >= 0; i--)
+	{
+		printf("%d\n", stack[i]);
+	}
+}
